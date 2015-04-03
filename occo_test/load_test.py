85fdf85ffe052f6fca1413cb78b294c008ae3c00
Novak Adam <novak.adam@sztaki.mta.hu>
2015-04-03 09:07:23 +0000

    update unittests

--- occo_test/load_test.py
+++ occo_test/load_test.py
@@ -3,14 +3,15 @@
 import unittest
 import occo.compiler as compiler
 import yaml
+import occo.util as util
 
-def gen_test_load_dict(infra_desc):
+def gen_case_load_dict(infra_desc):
     def test(self):
         statd = compiler.StaticDescription(infra_desc)
         self.assertEqual(yaml.load(str(statd.topological_order)),
                          infra_desc['expected_output'])
     return test
-def gen_test_load_str(infra_desc):
+def gen_case_load_str(infra_desc):
     infra_description = yaml.dump(infra_desc)
     def test(self):
         statd = compiler.StaticDescription(infra_description)
@@ -21,10 +22,10 @@ def gen_test_load_str(infra_desc):
 class CompilerTest(unittest.TestCase):
     pass
 
-with open('test-config.yaml') as f:
+with open(util.rel_to_file('test-config.yaml')) as f:
     config = yaml.load(f)
 for isdesc in config['infrastructures']:
     setattr(CompilerTest, 'test_load_dict_%s'%isdesc['name'],
-            gen_test_load_dict(isdesc))
+            gen_case_load_dict(isdesc))
     setattr(CompilerTest, 'test_load_str_%s'%isdesc['name'],
-            gen_test_load_str(isdesc))
+            gen_case_load_str(isdesc))
