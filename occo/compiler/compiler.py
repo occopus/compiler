c3ebb1e2affcb71a7e1fdb7ce6e591c9001ae9e8
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2015-07-08 16:28:39 +0200

    Adding checking for circle

--- occo/compiler/compiler.py
+++ occo/compiler/compiler.py
@@ -310,7 +310,10 @@ class StaticDescription(object):
             ## Now it's simple: independent = all \ dependent
             ## These nodes constitute a topological level
             topo_level = TopoLevel(n for n in nodes if not n in dependents)
-
+            # if topo_level is empty while nodes are not, there must be a circle
+            # among the nodes through the edges
+            if not topo_level :
+                raise SchemaError
             # Remove nodes that were put in this topological level, as now they
             # are satisfied dependencies.
             nodes = [n for n in nodes if not n in topo_level]
