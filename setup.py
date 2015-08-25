2f1a17b4989444d777b77888e3f2a2d03ed3b0ec
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-08-25 12:21:54 +0000

    Tidy requirements

--- setup.py
+++ setup.py
@@ -8,15 +8,20 @@ setuptools.setup(
     version='0.1.0',
     author='Adam Visegradi',
     author_email='adam.visegradi@sztaki.mta.hu',
-    namespace_packages=['occo'],
-    packages=['occo.compiler'],
-#    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    namespace_packages=[
+        'occo',
+    ],
+    packages=[
+        'occo.compiler',
+    ],
     url='http://www.lpds.sztaki.hu/',
     license='LICENSE.txt',
     description='OCCO Infrastructure Compiler Module',
     long_description=open('README.txt').read(),
-    install_requires=['PyYAML',
-                      'argparse',
-                      'dateutils',
-                      'OCCO-Util'],
+    install_requires=[
+        'argparse',
+        'python-dateutil',
+        'PyYAML',
+        'OCCO-Util',
+    ],
 )
