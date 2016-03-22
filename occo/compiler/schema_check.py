4313ba8e1045994ce610b44c8e3dc822fc47ad44
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2016-03-22 16:59:21 +0100

    Update schema checker and messages

--- occo/compiler/schema_check.py
+++ occo/compiler/schema_check.py
@@ -16,31 +16,32 @@ class SchemaChecker(object):
     @staticmethod
     def check_node_def(node_defs):
         for nodename, node_def in node_defs.iteritems():
-            if nodename.split(':')[0] is not 'node_def':
-                context = "[SCHEMA] ERROR in node %r - " % nodename
-                msg = "wrong node_definition name format"
+            realnodename = nodename.split(':')[1]
+            if nodename.split(':')[0] != 'node_def':
+                context = "[SchemaCheck] ERROR in node %r: " % realnodename
+                msg = "Node definition must begin with 'node_def:<nodename>'!"
                 raise SchemaError(msg, context)
-            if node_def is not list:
-                context = "[SCHEMA] ERROR in node %r - " % nodename
-                msg = "node_definition has to be a list of dicts"
+            if type(node_def) != list:
+                context = "[SchemaCheck] ERROR in node %r: " % realnodename
+                msg = "Node definition has to be a list of dictionaries!"
                 raise SchemaError(msg, context)
             for node in node_def:
                 #check for invalid sections:
-                for key in node
-                    if key not in ["resource", "config-management",
+                for key in node:
+                    if key not in ["resource", "config_management",
                             "contextualisation", "health_check"]:
-                        context = "[SCHEMA] ERROR in node %r - " % nodename
-                        msg = "invalid section %r" % key
+                        context = "[SchemaCheck] ERROR in node %r: " % realnodename
+                        msg = "Invalid section %r" % key
                         raise SchemaError(msg, context)
                 nodeindex = node_def.index(node)
                 #resource section
                 try:
                     if 'resource' not in node:
-                        raise SchemaError("missing RESOURCE section")
+                        raise SchemaError("Missing 'resource' section!")
                     else:
                         resource = node['resource']
                         if 'type' not in resource:
-                            raise SchemaError("missing key \'type\'")
+                            raise SchemaError("Missing key \'type\'")
                         else:
                             protocol = resource['type']
                             libname = "occo.plugins.resourcehandler." + protocol
@@ -48,22 +49,22 @@ class SchemaChecker(object):
                             checker = RHSchemaChecker.instantiate(protocol=protocol)
                             checker.perform_check(resource)
                 except SchemaError as e:
-                    context = "[SCHEMA] ERROR in resource section of node %r[%d] - " % (nodename, nodeindex)
+                    context = "[SchemaCheck] ERROR in 'resource' section of node %r[%d]: " % (realnodename, nodeindex)
                     raise SchemaError(e.msg, context)
                 #config_manager section
                 try:
                     if 'config_management' in node:
                         cm = node['config_management']
                         if 'type' not in cm:
-                            raise SchemaError("missing key \'type\'")
+                            raise SchemaError("Missing key \'type\'!")
                         else:
-                            protocol = resource['type']
+                            protocol = cm['type']
                             libname = "occo.plugins.configmanager." + protocol
                             importlib.import_module(libname)
                             checker = CMSchemaChecker.instantiate(protocol=protocol)
                             checker.perform_check(cm)
                 except SchemaError as e:
-                    context = "[SCHEMA] ERROR in config_management section of node %r[%d] - " % (nodename, nodeindex)
+                    context = "[SchemaCheck] ERROR in 'config_management' section of node %r[%d]: " % (realnodename, nodeindex)
                     raise SchemaError(e.msg, context)
                 
                 #contextualization section
@@ -71,7 +72,7 @@ class SchemaChecker(object):
                     if 'contextualisation' in node:
                         cont = node['contextualisation']
                         if 'type' not in cont:
-                            raise SchemaError("missing key \'type\'")
+                            raise SchemaError("Missing key \'type\'!")
                         else:
                             protocol = cont['type']
                             libname = "occo.plugins.infraprocessor.node_resolution." + protocol
@@ -79,7 +80,7 @@ class SchemaChecker(object):
                             checker = ContextSchemaChecker.instantiate(protocol=protocol)
                             checker.perform_check(cont)
                 except SchemaError as e:
-                    context = "[SCHEMA] ERROR in contextualisation section of node %r[%d] - " % (nodename, nodeindex)
+                    context = "[SchemaCheck] ERROR in 'contextualisation' section of node %r[%d]: " % (realnodename, nodeindex)
                     raise SchemaError(e.msg, context)
                         
                 #health_check section
