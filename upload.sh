2969e5d111318ac5212329a735a3ee66fc33b05e
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2020-04-24 13:55:38 +0000

    Fix package.sh and upload.sh

--- upload.sh
+++ upload.sh
@@ -10,4 +10,4 @@ if [ ! -d wheelhouse ]; then
     fi
 fi
 
-scp wheelhouse/OCCO?Compiler*.whl root@c155-10.localcloud:/opt/pypi-server/packages
+scp wheelhouse/OCCO?Compiler*.whl ubuntu@192.168.155.11:/opt/packages
